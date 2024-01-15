14aceb087e225aca3f4c43254a74e328c56c24ca
erkantokmak <erkan.tokmak.ofc@gmail.com>
2024-01-15 14:32:08 +0300

    Node.Js Tutorials 01

---
+++ 01TUT/server.js
@@ -0,0 +1,34 @@
+// You should already know : HTML, CSS, Javascript
+//How NodeJS differs from Vanilla JS
+// 1) Node runs on a server - not in a browser
+// 2) The console is the terminal window
+console.log('Hello World')
+// 3) global object instead of window object
+console.log(global)
+// 4) Has Common Core modules that we will explore
+// 5) CommonJS module instead of ES6 modules
+// 6) Missing some JS APIs like fetch
+
+const os = require('os')
+const path = require('path')
+const math = require('./math') 
+ /* or */ 
+const {add, subtract, multiply, divide} = require('./math')
+
+console.log(math.add(2,3))
+console.log(add(2,3))
+console.log(subtract(2,3))
+console.log(multiply(2,3))
+console.log(divide(2,3))
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))
